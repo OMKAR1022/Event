f9dedb4cb13142c9a06704db61e17afd4caed44e
Omkar <oholomkar40@gmail.com>
2025-02-10 23:58:33 +0530

    event analytics in proper structure now

---
+++ lib/ui/widgets/analytics/loading_state.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../../../../utils/app_colors.dart';
+
+class LoadingState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.primery),
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Loading analytics...',
+            style: TextStyle(
+              color: Colors.grey[600],
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
